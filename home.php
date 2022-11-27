c12b041481e9768fe7b8634bcefb3d728224c907
Ludavics <119253041+Ludavics@users.noreply.github.com>
2022-11-27 14:44:18 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">Silva F&D.</a>
+
+   <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="package.php">Package</a>
+      <a href="book.php">Book</a>
+      <a href="register.php">Signup</a>
+      <a href="register.php">Login</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+
+<!-- home section starts  -->
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/bkg.jpg) no-repeat">
+            <div class="content">
+               <span style="color: #f0f8ff; font-weight: bold; text-shadow: 4px 4px black; font-size: 40px;">explore, discover, trend</span>
+               <h3>SILVA DESIGNS & FASHION WORLD</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/bkg4.jpg) no-repeat">
+            <div class="content">
+               <span style="color: #f0f8ff; font-weight: bold; text-shadow: 4px 4px black; font-size: 40px;">explore, discover, trend</span>
+               <h3>discover the new trends</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/bkg4.jpg) no-repeat">
+            <div class="content">
+               <span style="color: #f0f8ff; font-weight: bold; text-shadow: 4px 4px black; font-size: 40px;">explore, discover, trend</span>
+               <h3>make your look worthwhile</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- services section starts  -->
+
+<section class="services">
+
+   <h1 class="heading-title"> our services </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/training.jpg" alt="">
+         <h3>trainings</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/show.jpg" alt="">
+         <h3>shows</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/ads.jpg" alt="">
+         <h3>advertisements</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/prom.jpg" alt="">
+         <h3>promosions</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/tailor1.jpg" alt="">
+         <h3>tailoring</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/marketing.jpg" alt="">
+         <h3>affiliation</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- home about section starts  -->
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="images/abbt.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>about us</h3>
+      <p>Silva fashions and designs is a platform created solely to connect us with our clients and customers across the globe, 
+      we deal in fashion and designing of new outfits, 
+      we also offer training sessions for students and do fashion shows often.</p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+<!-- home packages section starts  -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> our packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/training.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>trainings</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, sint!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/show.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>fashion shows</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, sint!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/sketch2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>creating & designing</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, sint!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+</section>
+
+<!-- home packages section ends -->
+
+<!-- home offer section starts  -->
+
+<section class="home-offer">
+   <div class="content">
+      <h3>upto 50% off</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure tempora assumenda, debitis aliquid nesciunt maiores quas! Magni cumque quaerat saepe!</p>
+      <a href="book.php" class="btn">book now</a>
+   </div>
+</section>
+
+<!-- home offer section ends -->
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +256-776-299-456 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +256-701-884-503 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> sannivesilvva@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Kampala, Uganda - 1469 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mr. sanive silva</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ package.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">silva f&d.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+      <a href="signup.php">Signup</a>
+      <a href="login.php">Login</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/bkg.jpg) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top services</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/training.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>training sessions</h3>
+            <p>we offer both physical and online training sessions!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/show.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>fashion shows</h3>
+            <p>we do fashions two to three times a year!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/tailor1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>tailoring</h3>
+            <p>we do tailoring trainings for physical students!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/sketch2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>creating & designing</h3>
+            <p>creating and designing is our priority!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/ads.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>advertisements</h3>
+            <p>we offer advertisement activities both on our website and popular media platforms!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/prom.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>promotions</h3>
+            <p>we do promosions for our clients !</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/marketing.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>affiliation</h3>
+            <p>we do affiliate marketing!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/bkg2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>selling</h3>
+            <p>we also sell our customer's stocks both on our website and our stores!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/bkg3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>trends</h3>
+            <p>we offer our customers new trending fashion and designs!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      
+   </div>
+
+   <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +256-776-299-456 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +256-701-884-503 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> sannivesilvva@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Kampala, Uganda - 1469 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mr. sanive silva</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
